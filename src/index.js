cce4eb0ca09e79effa4738dfe5c43b37894d32ff
luochang <173408798@qq.com>
2020-08-05 17:05:17 +0800

    添加 .gitignore

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // "11111";
 // 为什么要分析依赖，因为我们需要拿到路径
-import { add } from "./a.js";
-import { test } from "./b.js";
-console.log(add(1, 2));
-test("webpack");
-console.log("asdfasd");
+// import { add } from "./a.js";
+// import { test } from "./b.js";
+// console.log(add(1, 2));
+// test("webpack");
+// console.log("asdfasd");
 
-// 异步加载
-import(/*webpackChunkName:"async"*/ "./async.js").then((res) => {
-  // 执行 show 函数
-  res.show("webpackJsonp");
-});
+// // 异步加载
+// import(/*webpackChunkName:"async"*/ "./async.js").then((res) => {
+//   // 执行 show 函数
+//   res.show("webpackJsonp");
+// });

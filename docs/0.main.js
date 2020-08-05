5d6d0a45625069a0bcf6e23e969bc68a61a818ac
luochang <173408798@qq.com>
2020-08-05 17:20:24 +0800

    添加 build和docs文件夹

---
+++ docs/0.main.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/async.js":
+/*!**********************!*\
+  !*** ./src/async.js ***!
+  \**********************/
+/*! exports provided: show */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"show\", function() { return show; });\nconst show = (name) => {\n  console.log(\"nihao\", name);\n};\n\n\n//# sourceURL=webpack:///./src/async.js?");
+
+/***/ })
+
+}]);
